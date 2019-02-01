a1078a1f31d926415016a892de8f82e559b499da
Alex Rey <alex.rey@birchmangroup.com>
2019-02-01 13:49:44 +0100

    First commit

---
+++ star-wars-wiki/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavMenu from './components/nav-menu.component';
+
+class App extends Component {
+  render() {
+    return (
+      <NavMenu />
+    );
+  }
+}
+
+export default App;
